1ab8163b7f1a7ea5b73e9d9385af071fa9620cae
alekmatyka <amatyka@student.agh.edu.pl>
2024-11-06 19:07:46 +0100

    strona

--- formscript.js
+++ formscript.js
@@ -1,8 +1,9 @@
-const form = document.querySelector("form")
 const select = document.getElementById("temat")
-const textinput = form.querySelector("textarea")
 const textarea = document.getElementById("textarea")
+const form = document.querySelector("form")
+const textinput = form.querySelector("textarea")
 const emailinput = form.querySelector("input[name=email]")
+const formMessage = form.querySelector(".form-message")
 
 const placeholders={
     prayer:"Panie Boże Imperatorze",
@@ -14,4 +15,32 @@ textarea.setAttribute("placeholder",placeholders[select.value])
 
 select.addEventListener("click",e=>{
     textarea.setAttribute("placeholder",placeholders[select.value])
+})
+
+//weryfikacja
+const regEmail = /\S+@\S+\.\S+/;
+
+form.addEventListener("submit",e => {
+    e.preventDefault();
+
+    let formErrors = [];
+    if(textinput.value.length<=0){
+        formErrors.push("Wypełnij poprawnie pole z wiadomością");
+    }
+
+    if (!regEmail.test(emailinput.value)) {
+        formErrors.push("Wypełnij poprawnie pole z emailem");
+    }
+
+    if (!formErrors.length) {
+        form.submit();
+    } else {
+        formMessage.innerHTML = `
+            <h3 class="form-error-title">Przed wysłaniem proszę poprawić błędy:</h3>
+            <ul class="form-error-list">
+                ${formErrors.map(el => `<li>${el}</li>`).join("")}
+            </ul>
+        `;
+    }
+
 })
